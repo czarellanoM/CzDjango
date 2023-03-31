b269b6dc87372894fe837d15cb65bacf31e04853
czarellanoM <73495606+czarellanoM@users.noreply.github.com>
2023-03-31 11:06:05 -0500

    index

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("You are on the main page of Platzi App awards")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
 
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
+
 def results(request, question_id):
     return HttpResponse("You're seeing the results of question number %s." % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
